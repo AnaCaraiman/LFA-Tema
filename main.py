2c6fa87c13f2d534cdef3d322f5eabf6d6023841
AnaCaraiman <127100736+AnaCaraiman@users.noreply.github.com>
2023-03-27 05:32:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+dautomat={} #dicitonar in dictionar pentru automat
+starifinale=[]
+drum=['q0']#starea initiala
+starecurenta='q0'
+sw=0
+f = open('exemplu.in', 'r')
+linie=f.readlines()
+for i in range(len(linie)-1):
+    linie[i]=[x for x in linie[i].split()]
+    if linie[i][0] not in dautomat.keys():
+        dautomat[linie[i][0]]={linie[i][1]: linie[i][2]}
+
+    elif linie[i][1] in dautomat[linie[i][0]]:
+        print("Nu este AFD, este un AFN")
+        sw=1
+    else:
+        dautomat[linie[i][0]][linie[i][1]]=linie[i][2]
+starifinale=[i for i in linie[len(linie)-1].split()]
+
+if sw==0:
+  cuvant=input('Cuvantul testat: ')
+  cuvant=[i for i in cuvant]
+
+  for i in range(len(cuvant)):
+    if cuvant[i] in dautomat[starecurenta]:
+      drum.append(dautomat[starecurenta][cuvant[i]])
+      starecurenta=drum[len(drum)-1]
+    else:
+        print("Nu este acceptat")
+  if starecurenta in starifinale:
+      print("Drumul:", *drum)
+  else:
+      print("Nu este acceptat")
+
+

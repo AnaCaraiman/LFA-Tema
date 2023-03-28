55dc82c58d790f8e0daaa2d304382c997fa198bf
AnaCaraiman <anacaraiman51@gmail.com>
2023-03-28 14:41:34 +0300

    Fin

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 dautomat={} #dicitonar in dictionar pentru automat
 starifinale=[]
-drum=['q0']#starea initiala
-starecurenta='q0'
+
 sw=0
 f = open('exemplu.in', 'r')
 linie=f.readlines()
-for i in range(len(linie)-1):
+x=[i for i in linie[0].split()]
+for i in range(1,len(linie)-1):
     linie[i]=[x for x in linie[i].split()]
     if linie[i][0] not in dautomat.keys():
         dautomat[linie[i][0]]={linie[i][1]: linie[i][2]}
@@ -20,16 +20,18 @@ starifinale=[i for i in linie[len(linie)-1].split()]
 if sw==0:
   cuvant=input('Cuvantul testat: ')
   cuvant=[i for i in cuvant]
-
-  for i in range(len(cuvant)):
-    if cuvant[i] in dautomat[starecurenta]:
-      drum.append(dautomat[starecurenta][cuvant[i]])
-      starecurenta=drum[len(drum)-1]
-    else:
-        print("Nu este acceptat")
-  if starecurenta in starifinale:
+for z in x:
+    starecurenta=z
+    drum=[z]
+    for i in range(len(cuvant)):
+        if cuvant[i] in dautomat[starecurenta]:
+          drum.append(dautomat[starecurenta][cuvant[i]])
+          starecurenta=drum[len(drum)-1]
+        else:
+          print("Nu este acceptat")
+    if starecurenta in starifinale:
       print("Drumul:", *drum)
-  else:
-      print("Nu este acceptat")
+    else:
+       print("Nu este acceptat")
 
 
